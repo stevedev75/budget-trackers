7a8968aa1ca8bb41b30b61c8799eff7187aadec3
Steve Dever <stephendever.cr@gmail.com>
2021-07-17 17:23:11 -0400

    deploying again

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//mongoose.connect("mongodb://localhost/budget", {
+ // useNewUrlParser: true,
+//  useFindAndModify: false
+//});
 
 // routes
 app.use(require("./routes/api.js"));
