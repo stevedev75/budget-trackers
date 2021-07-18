24d5749de6eaa9abbef3b33952194abc77940138
Steve Dever <stephendever.cr@gmail.com>
2021-07-18 11:23:19 -0400

    deploying

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-// const PORT = 3000;
+ //const PORT = 3000;
 
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/fathomless-citadel-15629',
+  process.env.MONGODB_URI || 'mongodb://localhost/shielded-ridge-87332',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -28,10 +28,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//mongoose.connect("mongodb://localhost/budget", {
- // useNewUrlParser: true,
+// mongoose.connect("mongodb://localhost/budget", {
+//  useNewUrlParser: true,
 //  useFindAndModify: false
-//});
+// });
 
 // routes
 app.use(require("./routes/api.js"));
